a8683c002e14345062b997142dd7c6c52b12bb2e
jacksonlake <jax.lake.ltd@gmail.com>
2018-08-07 13:10:35 +0300

    Works, but there is no error handling

--- acesimserver.py
+++ acesimserver.py
@@ -3,6 +3,7 @@ import numpy as np
 import getopt
 import os.path
 import subprocess
+import csv
 
 def check_existence(FILENAME):
     if os.path.isfile(FILENAME) == False:
@@ -10,6 +11,29 @@ def check_existence(FILENAME):
         sys.exit(2)
     else:
         return True
+
+def read_csv_audioset(file):
+    with open(file, newline='') as csvfile:
+        fileobject = csv.reader(csvfile, delimiter=',', quotechar='|') #delimeter for normal file is ','!
+        youtube_id_list = []
+        star_time_list = []
+        stop_time_list = []
+        for row in fileobject: 
+            youtube_id_list.append(row[0])
+            star_time_list.append(row[1])
+            stop_time_list.append(row[2])
+        
+        star_time_list = star_time_list[3:]
+        stop_time_list = stop_time_list[3:]
+        youtube_id_list = youtube_id_list[3:]
+
+        youtube_id_list = ' '.join(youtube_id_list).replace(',','').split()
+        star_time_list = ' '.join(star_time_list).replace(',','').split()
+        stop_time_list = ' '.join(stop_time_list).replace(',','').split()
+        return youtube_id_list,star_time_list,stop_time_list
+
+        
+
 
 def download_crop_wav(name,START_TIME,STOP_TIME,link):
 #--------------------------------------------------
@@ -40,12 +64,12 @@ def download_crop_wav(name,START_TIME,STOP_TIME,link):
     print('---------------------------\n')
     print('AI-ready file created...\n')
     print('---------------------------')
-    os.system('ls')
     os.system('rm -rf *m4a') #Clean-up: all M4A files should be removed
+    os.system('ls')
 
 def main(argv):
     inputfile=''
-    outputfile=''
+    #outputfile=''
     try:
         opts, args = getopt.getopt(argv,"hi:",["ifile="])
     except getopt.GetoptError:
@@ -58,15 +82,21 @@ def main(argv):
         elif opt in ("-i", "--ifile"):
             inputfile = arg
     outputfile = '_' + inputfile
+    csvfile = 'balanced_train_segments.csv'
     check_existence(inputfile)
-    os.system('rm -rf *m4a') #Before we start, all M4A files should be removed
+    check_existence(csvfile)
+
+    youtube_id_list,star_time_list,stop_time_list = read_csv_audioset('balanced_train_segments.csv')
+    for i in range(len(youtube_id_list)):
+        download_crop_wav(str(i),star_time_list[i],stop_time_list[i],'https://www.youtube.com/watch?v=' + youtube_id_list[i])
+
     #-----names
-    START_TIME = str(50)
-    STOP_TIME = str(70)
-    link = 'https://www.youtube.com/watch?v=MkgR0SxmMKo'
-    name = 'single'
+    #START_TIME = str(50)
+    #STOP_TIME = str(70)
+    #link = 'https://www.youtube.com/watch?v=MkgR0SxmMKo'
+    #name = 'single'
     #-----names
-    download_crop_wav('double','70','90','https://www.youtube.com/watch?v=VDHFK5b2uWw')
+    #download_crop_wav('double','70','90','https://www.youtube.com/watch?v=c9cZSLfh7Xw')
     #download_crop_wav(name,START_TIME,STOP_TIME,link)
     
 
